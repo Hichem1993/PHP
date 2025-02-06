9a445fbff90e6ce778f007e20484b5cd5608dc69
Hichem-BEN-AYED <benayedhicham@gmail.com>
2025-02-06 09:07:16 +0100

    Projet

---
+++ Jour07/Projet/PHP/index.php
@@ -0,0 +1,14 @@
+<?php 
+// http://192.168.56.11/Jour07/Projet/PHP/index.php
+declare(strict_types = 1);
+
+
+
+if(!empty($_POST)){
+    var_dump($_POST); 
+}
+
+
+require_once "vue/header.php";
+require_once "vue/formulaire.php";
+require_once "vue/footer.php" ; 
